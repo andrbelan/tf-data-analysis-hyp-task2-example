45458d75f7f06b30a0ea085b646c8c85e9db08a0
andrbelan <130162109+andrbelan@users.noreply.github.com>
2023-04-13 21:21:12 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,14 @@
-import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import scipy.stats as st
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 412238846 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+  pv = st.cramervonmises_2samp(x, y)[1]
+  print(pv)
+  if pv > 0.04:
+    return False
+  else:
+    return True
